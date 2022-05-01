a82520ea69acca1c03fb1f6d531221d354bfd514
hugo minguez bonet <hugomin@hotmail.es>
2022-05-01 21:47:43 +0200

    Agregar archivos de proyecto.

---
+++ ProyectoSorting/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProyectoSorting
+{
+    public class SortingArray
+    {
+        public int[] array;
+        public int[] arrayCreciente;
+        public int[] arrayDecreciente;
+
+        public SortingArray(int elements, Random random)
+        {
+            array = new int[elements];
+            arrayCreciente = new int[elements];
+            arrayDecreciente = new int[elements];
+            for (int i = 0; i < elements; i++)
+            {
+                array[i] = random.Next(100);
+            }
+            Array.Copy(array, arrayCreciente, elements);
+            Array.Sort(arrayCreciente);
+            Array.Copy(arrayCreciente, arrayDecreciente, elements);
+            Array.Reverse(arrayDecreciente);
+        }
+
+        public void Sort(Action<int[]> func)
+        {
+            Stopwatch time = new Stopwatch();
+            int[] temp = new int[array.Length];
+            Array.Copy(array, temp, array.Length);
+
+            Console.WriteLine(func.Method.Name);
+
+            time.Start();
+
+            func(temp);
+
+            time.Stop();
+
+            Console.WriteLine("Initial: " + time.ElapsedMilliseconds + "ms " + time.ElapsedTicks + "ticks");
+
+            time.Reset();
+
+            time.Start();
+
+            func(temp);
+
+            time.Stop();
+
+            Console.WriteLine("Increasing: " + time.ElapsedMilliseconds + "ms " + time.ElapsedTicks + "ticks");
+
+            time.Reset();
+
+            Array.Reverse(temp);
+
+            time.Start();
+
+            func(temp);
+
+            time.Stop();
+
+            Console.WriteLine("Decreasing: " + time.ElapsedMilliseconds + "ms " + time.ElapsedTicks + "ticks");
+        }
+
+        public void mergeSort(int[] arr, int l, int m, int r)
+        {
+            
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+            
+            i = 0;
+            j = 0;
+
+           
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        public void InsertionSort(int[] array)
+            {
+                int n = array.Length;
+                for (int i = 1; i < n; ++i)
+                {
+                    int key = array[i];
+                    int j = i - 1;
+
+                    // Move elements of arr[0..i-1],
+                    // that are greater than key,
+                    // to one position ahead of
+                    // their current position
+                    while (j >= 0 && array[j] > key)
+                    {
+                        array[j + 1] = array[j];
+                        j = j - 1;
+                    }
+                    array[j + 1] = key;
+                }
+            }
+        
+        public void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public void BubbleSortEarlyExit(int[] array)
+        {
+            bool ordered = true;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                ordered = true;
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        ordered = false;
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+                if (ordered)
+                    return;
+            }
+        }
+        public void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+        public void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = QuickSortPivot(array, left, right);
+                QuickSort(array, left, pivot);
+                QuickSort(array, pivot + 1, right);
+            }
+        }
+        public int QuickSortPivot(int[] array, int left, int right)
+        {
+            int pivot = array[(left + right) / 2];
+            while (true)
+            {
+                while (array[left] < pivot)
+                {
+                    left++;
+                }
+                while (array[right] > pivot)
+                {
+                    right--;
+                }
+                if (left >= right)
+                {
+                    return right;
+                }
+                else
+                {
+                    int temp = array[left];
+                    array[left] = array[right];
+                    array[right] = temp;
+                    right--; left++;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many elements do you want?");
+            int elements = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What seed do you want to use?");
+            int seed = int.Parse(Console.ReadLine());
+
+            Random random = new Random(seed);
+            SortingArray array = new SortingArray(elements, random);
+            array.Sort(array.mergeSort);
+            array.Sort(array.InsertionSort);
+            array.Sort(array.BubbleSort);
+            array.Sort(array.BubbleSortEarlyExit);
+            array.Sort(array.QuickSort);
+
+        }
+    }
+}
